f1d4326679186e7bb1b9cad28ffb3a5e656d531e
wilmil123 <61195187+wilmil123@users.noreply.github.com>
2025-07-28 11:49:54 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!bin/bash
 
-HOME_DIR=/vibrio_predictions_map/
-PY_OUT_DIR=/vibrio_predictions_map/env_params
-RF_MODELS_DIR=/vibrio_predictions_map/rf_models
-R_OUT_DIR=/vibrio_predictions_map/raster_predictions
+HOME_DIR=./
+PY_OUT_DIR=./env_params
+RF_MODELS_DIR=./rf_models
+R_OUT_DIR=./raster_predictions
 USERNAME=wmchapman@mta.ca
 PASSWORD=Hygrometer16!
 
-python3.12 ~/Documents/school/ecab/2025/vibrio_predictions_map/fetch_env_params.py --home_dir $HOME_DIR --out_dir $PY_OUT_DIR --raster_dir $R_OUT_DIR --user $USERNAME --pwd $PASSWORD
-Rscript ~/Documents/school/ecab/2025/vibrio_predictions_map/make_vib_predictions.R --raster_dir $PY_OUT_DIR --model_dir $RF_MODELS_DIR --out_dir $R_OUT_DIR
+python3.12 ./fetch_env_params.py --home_dir $HOME_DIR --out_dir $PY_OUT_DIR --raster_dir $R_OUT_DIR --user $USERNAME --pwd $PASSWORD
+Rscript ./make_vib_predictions.R --raster_dir $PY_OUT_DIR --model_dir $RF_MODELS_DIR --out_dir $R_OUT_DIR
