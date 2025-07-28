81d439603608fcd4d603d820a31af52d6df951c5
wilmil123 <61195187+wilmil123@users.noreply.github.com>
2025-07-28 14:50:26 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,6 @@ HOME_DIR=./
 PY_OUT_DIR=./env_params
 RF_MODELS_DIR=./rf_models
 R_OUT_DIR=./raster_predictions
-USERNAME=wmchapman@mta.ca
-PASSWORD=Hygrometer16!
 
 python3.12 ./fetch_env_params.py --home_dir $HOME_DIR --out_dir $PY_OUT_DIR --raster_dir $R_OUT_DIR --user $USERNAME --pwd $PASSWORD
 Rscript ./make_vib_predictions.R --raster_dir $PY_OUT_DIR --model_dir $RF_MODELS_DIR --out_dir $R_OUT_DIR
